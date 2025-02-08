3cc409ca1afd35f57b3f37bc48e82f7a6ccc6dfe
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-08 23:34:18 +0530

    Priority Queue

---
+++ StandardTemplateLibrary/priorirty_queue.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // max heap
+    priority_queue<int> pq;
+
+    pq.push(10);
+    pq.push(20);
+    pq.emplace(5);
+    pq.emplace(69);
+    
+    cout << pq.top() << "\n";
+    pq.pop();
+    cout << pq.top() << "\n";
+    
+    while (!pq.empty()) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << "\n";
+
+    priority_queue<int, vector<int>, greater<int>> pq1;
+    pq1.push(10);
+    pq1.push(20);
+    pq1.emplace(5);
+    pq1.emplace(69);
+    
+    cout << pq1.top() << "\n";
+    pq1.pop();
+    cout << pq1.top() << "\n";
+    
+    while (!pq1.empty()) {
+        cout << pq1.top() << " ";
+        pq1.pop();
+    }
+    cout << "\n";
+
+    return 0;
+}
