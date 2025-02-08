b7d18991774b758846dd1a83659e7c51151c594d
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-08 23:48:04 +0530

    Unordered set

---
+++ StandardTemplateLibrary/unordered_set.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unordered_set<int> st;
+
+    st.insert(21);
+    st.insert(56);
+    st.insert(21);
+    st.insert(69);
+    st.insert(7);
+
+    for (auto it : st) {
+        cout << it << " ";
+    }
+    cout << "\n";
+
+    cout << st.count(21) << "\n";
+
+    st.erase(21);
+
+    for (auto it : st) {
+        cout << it << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
