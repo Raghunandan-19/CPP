7e3e6e24cdd9f20c1867d1e88cb42a64cae0eaa6
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-10 19:39:44 +0530

    Array Basics

---
+++ BasicArrays/basics.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    
+    cout << arr[2] << "\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
