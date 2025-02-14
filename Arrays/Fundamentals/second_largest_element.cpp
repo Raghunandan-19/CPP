b0b4a1e602a8534313c7f6099e92cc8c4e538266
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-14 21:04:47 +0530

    Second Largest element

--- Arrays/Fundamentals/second_largest_element.cpp
+++ Arrays/Fundamentals/second_largest_element.cpp
@@ -4,12 +4,48 @@ using namespace std;
 class Solution {
 public:
     int secondLargestElement(vector<int> &nums) {
+        // size of the vector
         int n = nums.size();
+        // initializing largest and second largest element with INT_MIN
         int largest = INT_MIN;
         int secondLargest = INT_MIN;
 
+        // iterating through vector
         for (int i = 0; i < n; i++) {
-            
+            // if current element is greater than largest
+            if (nums[i] > largest) {
+                /* update the second largest element with largest element
+                and largest element with current element */
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            /* if current element is greater than second largest and not 
+            equal to largest element */
+            else if (nums[i] > secondLargest && nums[i] != largest) {
+                // update the second largest element with current element
+                secondLargest = nums[i];
+            }
         }
+
+        /* If second largest element is equal to INT_MIN return -1 else 
+        return second largest element */
+        return secondLargest == INT_MIN ? -1 : secondLargest;
     }
 };
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    Solution sol;
+    cout << sol.secondLargestElement(nums) << "\n";
+
+    return 0;
+}
