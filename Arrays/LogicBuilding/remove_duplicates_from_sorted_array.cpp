74485bb3876e0297e886333fa225b5b7de9e2aaf
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-15 22:57:40 +0530

    Added comments Remove duplicates from sorted array

--- Arrays/LogicBuilding/remove_duplicates_from_sorted_array.cpp
+++ Arrays/LogicBuilding/remove_duplicates_from_sorted_array.cpp
@@ -4,16 +4,23 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int> &nums) {
+        // size of the vector
         int n = nums.size();
+        // intializing i with 0
         int i = 0;
 
+        // iterating from i + 1 to n - 1 index
         for (int j = i + 1; j < n; j++) {
+            // if element at jth index is not equal to current element
             if (nums[j] != nums[i]) {
+                // updating element at i + 1 with current element
                 nums[i + 1] = nums[j];
+                // incrementing i by 1
                 i++;
             }
         }
 
+        // returning the number of unique elements
         return (i + 1);
     }
 };
