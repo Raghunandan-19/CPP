d60eb3f127791f507aef7309515a8319f860ae68
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-15 23:00:49 +0530

    Added comments to Move zeroes to end

--- Arrays/LogicBuilding/move_zeroes_to_end.cpp
+++ Arrays/LogicBuilding/move_zeroes_to_end.cpp
@@ -4,22 +4,33 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int> &nums) {
+        // size of the vector
         int n = nums.size();
 
+        // intializing zero index with -1
         int zeroIndex = -1;
 
+        // iterating through the vector
         for (int i = 0; i < n; i++) {
+            // if current element is 0
             if (nums[i] == 0) {
+                // updating zero index with current index
                 zeroIndex = i;
+                // breaking from the loop
                 break;
             }
         }
 
+        // if no zero is present in vecor returning from the function
         if (zeroIndex == -1) return;
 
+        // iterating from 0 index + 1 to n - 1
         for (int i = zeroIndex + 1; i < n; i++) {
+            // if current element is not equal to zero  
             if (nums[i] != 0) {
+                // swapping current element with zero
                 swap(nums[i], nums[zeroIndex]);
+                // incrementing the zero index by 1
                 zeroIndex++;
             }
         }
