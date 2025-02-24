f652206b4726a14db4d4e5d6162c4f8829a2a3bc
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-24 20:19:05 +0530

    Max subarray sum using kadane's algorithm

--- Arrays/FaqMedium/kadanes_algorithm.cpp
+++ Arrays/FaqMedium/kadanes_algorithm.cpp
@@ -5,18 +5,28 @@ class Solution {
 public:
     int maxSubArray(vector<int> &nums) {
         int n = nums.size();
-        int maxSum = INT_MIN;
 
+        // Initialize maxSum to the smallest possible value and sum to 0
+        long long maxSum = LLONG_MIN;
+        long long sum = 0;
+
+        // Iterate through the array
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += nums[k];
-                }
-                maxSum = max(maxSum, sum);
+            // Add the current element to the sum
+            sum += nums[i];
+
+            // Update maxSum if the current sum is greater than maxSum
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+
+            // If the current sum is negative, reset it to 0
+            if (sum < 0) {
+                sum = 0;
             }
         }
 
+        // Return the maximum sum found
         return maxSum;
     }
 };
