bd863e3470ec70012dc12c70a9188bead950de22
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-26 00:33:12 +0530

    Majority element 1 Optimal solution using Moore's voting algorithm

--- Arrays/FaqHard/majority_element_1.cpp
+++ Arrays/FaqHard/majority_element_1.cpp
@@ -5,19 +5,32 @@ class Solution {
 public:
     int majorityElement(vector<int> &nums) {
         int n = nums.size();
-        unordered_map<int, int> mpp;
+        int el; // Variable to store the potential majority element
+        int cnt = 0; // Counter to track the frequency of the potential majority element
 
-        for (auto &num : nums) {
-            mpp[num]++;
-        }
-
-        for (auto &el : mpp) {
-            if (el.second > (n / 2)) {
-                return el.first;
+        // Boyer-Moore Voting Algorithm to find the candidate for majority element
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i]; // Set the current element as the potential majority element
+            }
+            else if (el == nums[i]) {
+                cnt++; // Increment the counter if the current element matches the potential majority element
+            }
+            else {
+                cnt--; // Decrement the counter if the current element does not match the potential majority element
             }
         }
 
-        return -1;
+        int cnt1 = 0; // Counter to verify if the candidate is actually the majority element
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                cnt1++; // Count the occurrences of the candidate element
+            }
+        }
+
+        // Return the candidate if it is the majority element, otherwise return -1
+        return cnt1 > (n / 2) ? el : -1;
     }
 };
 
