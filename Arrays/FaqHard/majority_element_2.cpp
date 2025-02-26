9e91ed04a102b38adcef32d557f8b619aee2b1e3
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-26 14:56:06 +0530

    Majority element 2 Optimal solution using moore's voting algotith extended

--- Arrays/FaqHard/majority_element_2.cpp
+++ Arrays/FaqHard/majority_element_2.cpp
@@ -5,19 +5,57 @@ class Solution {
 public:
     vector<int> majorityElementTwo(vector<int> &nums) {
         int n = nums.size();
-        map<int, int> mpp;
+        int cnt1 = 0, cnt2 = 0;
+        int el1, el2;
 
-        for (int num : nums) {
-            mpp[num]++;
-        }
-
-        vector<int> ans;
-        for (auto &it : mpp) {
-            if (it.second > (n / 3)) {
-                ans.push_back(it.first);
+        // First pass to find potential majority elements
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && nums[i] != el2) {
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            else if (cnt2 == 0 && nums[i] != el1) {
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if (el1 == nums[i]) {
+                cnt1++;
+            }
+            else if (el2 == nums[i]) {
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
             }
         }
 
+        vector<int> ans;
+        cnt1 = 0, cnt2 = 0;
+
+        // Second pass to confirm the majority elements
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) {
+                cnt1++;
+            }
+
+            if (nums[i] == el2) {
+                cnt2++;
+            }
+        }
+
+        // Check if the elements appear more than n/3 times
+        if (cnt1 > (n / 3)) {
+            ans.push_back(el1);
+        }
+
+        if (cnt2 > (n / 3)) {
+            ans.push_back(el2);
+        }
+
+        // Sort the result before returning
+        sort(ans.begin(), ans.end());
+        
         return ans;
     }
 };
