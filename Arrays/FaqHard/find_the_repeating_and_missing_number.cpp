ade1247240d215f0094ec76773ee3c471fdc1781
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-27 23:39:46 +0530

    Find the repeating and missing number better solution

--- Arrays/FaqHard/find_the_repeating_and_missing_number.cpp
+++ Arrays/FaqHard/find_the_repeating_and_missing_number.cpp
@@ -6,19 +6,17 @@ public:
     vector<int> findMissingRepeatingNumbers(vector<int> nums) {
         int n = nums.size();
         int missing = -1, repeating = -1;
+        int hash[n + 1] = {0};
+
+        for (int i = 0; i < n; i++) {
+            hash[nums[i]]++;
+        }
 
         for (int i = 1; i <= n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == i) {
-                    cnt++;
-                }
-            }
-
-            if (cnt == 0) {
+            if (hash[i] == 0) {
                 missing = i;
             }
-            else if (cnt == 2) {
+            else if (hash[i] == 2) {
                 repeating = i;
             }
         }
