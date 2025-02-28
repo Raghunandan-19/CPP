83b2fe3e8861307bbe7201a090a97124db7866dc
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-02-28 09:36:31 +0530

    Count Inversions Brute force approach

---
+++ Arrays/FaqHard/count_inversions.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long int numberOfInversions(vector<int> nums) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] > nums[j]) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    Solution sol;
+    cout << sol.numberOfInversions(nums) << "\n";
+
+    return 0;
+}
