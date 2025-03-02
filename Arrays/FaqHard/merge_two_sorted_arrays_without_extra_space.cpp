371f87c71e0d6dcb6b44a64366e2a6ca9ebc48f5
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-02 23:41:25 +0530

    Merge two sorted arrays without extra space optimal approach

--- Arrays/FaqHard/merge_two_sorted_arrays_without_extra_space.cpp
+++ Arrays/FaqHard/merge_two_sorted_arrays_without_extra_space.cpp
@@ -2,31 +2,50 @@
 using namespace std;
 
 class Solution {
+private:
+    // Function to swap elements if the element in nums1 is greater than the element in nums2
+    void swapIfGreater(vector<int> &nums1, vector<int> &nums2, int ind1, int ind2) {
+        if (nums1[ind1] > nums2[ind2]) {
+            swap(nums1[ind1], nums2[ind2]);
+        }
+    }
+
 public:
+    // Function to merge two sorted arrays without extra space
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> merged(n + m);
-        int left = 0;
-        int right = 0;
-        int index = 0;
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
 
-        while (left < m && right < n) {
-            if (nums1[left] <= nums2[right]) {
-                merged[index++] = nums1[left++];
-            } else {
-                merged[index++] = nums2[right++];
+        // Using the gap method to sort the arrays
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // If left is in nums1 and right is in nums2
+                if (left < m && right >= m) {
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+                // If both left and right are in nums2
+                else if (left >= m) {
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+                // If both left and right are in nums1
+                else {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                left++;
+                right++;
             }
+
+            // Reduce the gap for the next iteration
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
 
-        while (left < m) {
-            merged[index++] = nums1[left++];
-        }
-
-        while (right < n) {
-            merged[index++] = nums2[right++];
-        }
-
-        for (int i = 0; i < (m + n); i++) {
-            nums1[i] = merged[i];
+        // Copy the elements of nums2 to nums1
+        for (int i = m; i < (m + n); i++) {
+            nums1[i] = nums2[i - m];
         }
     }
 };
