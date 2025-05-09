a6c0bbf6f30db970f8c2514a6fc9bdec5556c361
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-09 14:03:10 +0530

    Lower bound optimal using binary search

--- BinarySearch/Fundamentals/lower_bound.cpp
+++ BinarySearch/Fundamentals/lower_bound.cpp
@@ -4,13 +4,37 @@ using namespace std;
 class Solution {
 public:
     int lowerBound(vector<int> &nums, int x) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] >= x) {
-                return i;
+        // size of array
+        int n = nums.size();
+
+        // search space for binary search
+        int low = 0;
+        int high = n - 1;
+
+        // initializing answer with n if search space gets exhausted
+        int ans = n;
+
+        // applying binary search
+        while (low <= high) {
+            // mid index of search space
+            int mid = low + (high - low) / 2;
+
+            // check if mid element is >= x 
+            if (nums[mid] >= x) {
+                // updating answer as mid
+                ans =  mid;
+
+                // eliminating the right half
+                high = mid - 1;
+            }
+            // eliminating the left half
+            else {
+                low = mid + 1;
             }
         }
 
-        return nums.size();
+        // returning the index where lower bound of x is stored
+        return ans;
     }
 };
 
