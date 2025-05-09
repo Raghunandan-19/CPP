7be1772d5d17d953772e0edb1fbd6b1a5f197604
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-09 14:13:06 +0530

    Upper bound optimal solution using binary search

--- BinarySearch/Fundamentals/upper_bound.cpp
+++ BinarySearch/Fundamentals/upper_bound.cpp
@@ -4,13 +4,38 @@ using namespace std;
 class Solution {
 public:
     int upperBound(vector<int> &nums, int x) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > x) {
-                return i;
+        // Get the size of the vector
+        int n = nums.size(); 
+
+        // Initialize the lower bound and upper bound of the search range
+        int low = 0;         
+        int high = n - 1;   
+        
+        // Default answer is n (out of bounds)
+        int ans = n;         
+
+        // Perform binary search
+        while (low <= high) {
+            // Calculate the middle index to avoid overflow
+            int mid = low + (high - low) / 2; 
+
+            // If the middle element is greater than x, update the answer and search in the left half
+            if (nums[mid] > x) {
+                // Update the answer to the current index
+                ans = mid;    
+
+                // Move the upper bound to the left
+                high = mid - 1; 
+            }
+            // Otherwise, search in the right half
+            else {
+                // Move the lower bound to the right
+                low = mid + 1; 
             }
         }
 
-        return nums.size();
+        // Return the index of the first element greater than x
+        return ans; 
     }
 };
 
