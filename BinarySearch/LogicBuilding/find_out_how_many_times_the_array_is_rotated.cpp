afd96092f1fe03ce7d4f4f6b59770bd819df9123
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-12 21:58:35 +0530

    Find out how many times the array is rotated optimal using linear search'

--- BinarySearch/LogicBuilding/find_out_how_many_times_the_array_is_rotated.cpp
+++ BinarySearch/LogicBuilding/find_out_how_many_times_the_array_is_rotated.cpp
@@ -5,16 +5,64 @@ class Solution {
 public:
     int findKRotation(vector<int> &nums)  {
         int n = nums.size();
-        int idx = -1;
-        int minimum_element = INT_MAX;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minimum_element) {
-                minimum_element = nums[i];
-                idx = i;
+        // search space for binary search
+        int low = 0;
+        int high = n - 1;
+
+        // To store the index of the minimum element
+        int idx = -1; 
+
+        // Initialize minimum element to a very large value
+        int minimum_element = INT_MAX; 
+
+        while (low <= high) {
+            // Calculate mid to avoid overflow
+            int mid = low + (high - low) / 2; 
+
+            // If the subarray is already sorted
+            if (nums[low] <= nums[high]) {
+                if (nums[low] < minimum_element) {
+                    // Update minimum element
+                    minimum_element = min(nums[low], minimum_element); 
+                    
+                    // Update index of the minimum element
+                    idx = low; 
+                }
+
+                // Exit the loop as the minimum is found
+                break; 
+            }
+
+            // If the left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] < minimum_element) {
+                    // Update minimum element
+                    minimum_element = min(nums[low], minimum_element);
+                    
+                    // Update index of the minimum element
+                    idx = low; 
+                }
+
+                // Move to the right half
+                low = mid + 1; 
+            }
+            // If the right half is sorted
+            else {
+                if (nums[mid] < minimum_element) {
+                    // Update minimum element
+                    minimum_element = min(nums[mid], minimum_element); 
+                    
+                    // Update index of the minimum element
+                    idx = mid; 
+                }
+
+                // Move to the left half
+                high = mid - 1; 
             }
         }
 
+        // Return the index of the minimum element
         return idx;
     }
 };
