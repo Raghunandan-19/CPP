9cce6007b8f4583b4890cd8750559dbda4c0641f
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-17 13:14:25 +0530

    Find nth root of a number optimal using binary search

--- BinarySearch/OnAnswers/find_nth_root_of_a_number.cpp
+++ BinarySearch/OnAnswers/find_nth_root_of_a_number.cpp
@@ -2,19 +2,51 @@
 using namespace std;
 
 class Solution {
-public:
-    int nthRoot(int N, int M) {
-        for (int i = 1; i <= M; i++) {
-            long long val = pow(i, N);
+private:
+    long long power(int base, int exp, int M) {
+        long long result = 1;
 
-            if (val == M) {
-                return i;
-            }
-            else if (val > M) {
+        // Multiply base 'exp' times to compute base^exp
+        for (int i = 1; i <= exp; i++) {
+            result *= base;
+
+            // Early exit if result exceeds M to avoid unnecessary computation
+            if (result > M) {
                 break;
             }
         }
 
+        // Return the computed power (or early exit value)
+        return result;
+    }
+public:
+    int nthRoot(int N, int M) {
+        // Initialize the search range for the Nth root
+        int low = 1;
+        int high = M;
+
+        // Binary search to find the Nth root of M
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Compute mid^N
+            long long val = power(mid, N, M); 
+
+            if (val == M) {
+                // Found the Nth root
+                return mid;
+            }
+            else if (val < M) {
+                // mid^N is less than M, try higher values
+                low = mid + 1;
+            }
+            else {
+                // mid^N is greater than M, try lower values
+                high = mid - 1;
+            }
+        }
+
+        // If no integer Nth root exists, return -1
         return -1;
     }
 };
